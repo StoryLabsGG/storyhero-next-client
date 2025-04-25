2303e90ecd549792d7e64eb6eb23637cc4b05389
Ricky Liao <mrrickyliao@gmail.com>
2025-04-24 19:54:17 -0700

    build: add configurations for husky and commitlint

---
+++ commitlint.config.cjs
@@ -0,0 +1,8 @@
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'references-empty': [1, 'never'],
+    'footer-max-line-length': [0, 'always'],
+    'body-max-line-length': [0, 'always'],
+  },
+};
